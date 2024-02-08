ad3826db85fb0d1fb2a8e0537567df9092a130c7
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-08 16:13:36 +0100

    test: print hostname

--- test.ts
+++ test.ts
@@ -1,28 +1,34 @@
 import udp from "node:dgram";
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
 
+const hostname = Deno.env.get("HOSTNAME") ?? "localhost";
+const port = 2444;
+
 Deno.test("UDP echo server", async (t) => {
-  await t.step("the UDP server should echo a given string", async () => {
-    const msg = `Hello World (${Date.now()})!`;
+  await t.step(
+    `the UDP server at ${hostname}:${port} should echo a given string`,
+    async () => {
+      const msg = `Hello World (${Date.now()})!`;
 
-    const client = udp.createSocket("udp4");
+      const client = udp.createSocket("udp4");
 
-    const received = await new Promise<string>((resolve, reject) => {
-      const t = setTimeout(() => {
-        reject(new Error(`Timeout!`));
-        client.close();
-      }, 5000);
-      client.on("message", (msg: string) => {
-        clearTimeout(t);
-        console.log(`<`, msg.toString());
-        resolve(msg.toString());
-        client.close();
+      const received = await new Promise<string>((resolve, reject) => {
+        const t = setTimeout(() => {
+          reject(new Error(`Timeout!`));
+          client.close();
+        }, 5000);
+        client.on("message", (msg: string) => {
+          clearTimeout(t);
+          console.log(`<`, msg.toString());
+          resolve(msg.toString());
+          client.close();
+        });
+
+        console.log(`>`, msg);
+        client.send(msg, port, hostname, () => {});
       });
 
-      console.log(`>`, msg);
-      client.send(msg, 2444, Deno.env.get("HOSTNAME") ?? "localhost", () => {});
-    });
-
-    assertEquals(received.endsWith(msg), true);
-  });
+      assertEquals(received.endsWith(msg), true);
+    },
+  );
 });
