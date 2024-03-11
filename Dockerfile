76f1bcf1a79c1090f0b060921f4cddb8763a4c91
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-03-11 12:31:00 +0100

    feat: log event to Azure Monitor

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,16 @@ FROM ubuntu:latest
 
 ENV DEBIAN_FRONTEND=noninteractive
 
-RUN apt-get update && apt-get -yq install python3
+RUN <<EOT
+    apt-get update
+    apt-get -yq install python3 python3-pip
+    apt-get -y clean
+    rm -rf /var/lib/apt/lists/*
+    python3 -m pip install opencensus-ext-azure
+EOT
+
 COPY server.py /server.py
+
 EXPOSE 2444/udp
+
 CMD [ "python3", "/server.py"]
--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import os
 from _thread import * 
 import socket 
 import datetime 
+import logging
+from opencensus.ext.azure.log_exporter import AzureEventHandler
 
 localIP     = "::" 
 localPort   = 2444 
@@ -25,14 +27,23 @@ def multi_threaded_client(message,address):
     res = b'Time: '+str.encode(e.strftime("%Y-%m-%d %H:%M:%S"))+b' Message: '+ str.encode(message.decode('utf-8')) 
     UDPServerSocket.sendto(res, address) 
 
+# Send custom events
+logger = logging.getLogger(__name__)
+metrics_connection_string = os.environ.get('APPLICATIONINSIGHTS_CONNECTION_STRING')
+if metrics_connection_string is not None:
+    logger.addHandler(AzureEventHandler(connection_string=metrics_connection_string))
+logger.setLevel(logging.INFO)
+
 # Listen for incoming datagrams 
 while(True): 
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize) 
     message = bytesAddressPair[0] 
     address = bytesAddressPair[1] 
     clientMsg = "Message from Client:{}".format(message) 
-    clientIP  = "Client IP Address:{}".format(address) 
+    clientIP  = "Client IP Address:{}".format(address)
+    logger.info('UDP-echo')
     print(clientMsg) 
     print(clientIP) 
+
     # Sending a reply to client 
     start_new_thread(multi_threaded_client, (message,address )) 
